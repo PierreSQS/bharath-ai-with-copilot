42853ab9858810b0ce2e7055ea68643ae962b3ad
PierreSQS <nomo_messina@web.de>
2025-01-31 22:14:05 +0100

    Sec4_Chap22: created project

---
+++ clinicalsapi/src/test/java/com/bharath/patientclinicals/clinicalsapi/clinicalsapi/ClinicalsapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bharath.patientclinicals.clinicalsapi.clinicalsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ClinicalsapiApplicationTests {
+
+@Test
+ void contextLoads(ApplicationContext appCtx) {
+	 assertThat(appCtx).isNotNull();
+ }
+
+}
