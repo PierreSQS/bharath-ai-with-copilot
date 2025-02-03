f871ca49fe0c6b8711244745206017a688af25cb
PierreSQS <nomo_messina@web.de>
2025-02-03 09:17:22 +0100

    Sec4_ASSN5: configured logging

--- clinicalsapi/src/main/java/com/bharath/patientclinicals/clinicalsapi/controllers/PatientController.java
+++ clinicalsapi/src/main/java/com/bharath/patientclinicals/clinicalsapi/controllers/PatientController.java
@@ -3,6 +3,7 @@ package com.bharath.patientclinicals.clinicalsapi.controllers;
 import com.bharath.patientclinicals.clinicalsapi.entities.Patient;
 import com.bharath.patientclinicals.clinicalsapi.repositories.PatientRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/patients")
@@ -19,23 +21,27 @@ public class PatientController {
 
     @GetMapping
     public List<Patient> getAllPatients() {
+        log.info("Getting all patients");
         return patientRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Patient> getPatientById(@PathVariable Long id) {
+        log.info("Getting patient by id: {}", id);
         Optional<Patient> patient = patientRepository.findById(id);
         return patient.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public ResponseEntity<Patient> createPatient(@RequestBody Patient patient) {
+        log.info("Creating patient: {}", patient);
         Patient savedPatient = patientRepository.save(patient);
         return new ResponseEntity<>(savedPatient, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Patient> updatePatient(@PathVariable Long id, @RequestBody Patient patientDetails) {
+        log.info("Updating patient with id: {}", id);
         Optional<Patient> optionalPatient = patientRepository.findById(id);
         if (optionalPatient.isPresent()) {
             Patient patient = optionalPatient.get();
@@ -51,6 +57,7 @@ public class PatientController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePatient(@PathVariable Long id) {
+        log.info("Deleting patient with id: {}", id);
         Optional<Patient> patient = patientRepository.findById(id);
         if (patient.isPresent()) {
             patientRepository.delete(patient.get());
