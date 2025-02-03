94c7407a184a3e8cc46f51e2fc8da99e65b409e6
PierreSQS <nomo_messina@web.de>
2025-02-03 07:24:55 +0100

    Sec4_Chap30-XXX: added a TimeStamp annotation

--- clinicalsapi/src/main/java/com/bharath/patientclinicals/clinicalsapi/entities/ClinicalData.java
+++ clinicalsapi/src/main/java/com/bharath/patientclinicals/clinicalsapi/entities/ClinicalData.java
@@ -1,21 +1,21 @@
 package com.bharath.patientclinicals.clinicalsapi.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.Builder;
-import lombok.Data;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Column;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -37,7 +37,7 @@ public class ClinicalData {
     @Column(name = "component_value")
     private String componentValue;
 
-    @CreationTimestamp
+    @UpdateTimestamp
     private LocalDateTime measuredDateTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
