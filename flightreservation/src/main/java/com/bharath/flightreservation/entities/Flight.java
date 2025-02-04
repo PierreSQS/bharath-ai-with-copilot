bf4b2b8eecbece5f9791da3084e13654fb1e6a2f
PierreSQS <nomo_messina@web.de>
2025-02-04 07:30:10 +0100

    Sec6_Chap45: refined generated entities

--- flightreservation/src/main/java/com/bharath/flightreservation/entities/Flight.java
+++ flightreservation/src/main/java/com/bharath/flightreservation/entities/Flight.java
@@ -1,16 +1,15 @@
 package com.bharath.flightreservation.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 
 @Data
@@ -27,6 +26,6 @@ public class Flight {
     private String operatingAirline;
     private String departureCity;
     private String arrivalCity;
-    private Date dateOfDeparture;
-    private Date estimatedDepartureTime;
+    private LocalDateTime dateOfDeparture;
+    private LocalDateTime estimatedDepartureTime;
 }
--- flightreservation/src/main/java/com/bharath/flightreservation/entities/Reservation.java
+++ flightreservation/src/main/java/com/bharath/flightreservation/entities/Reservation.java
@@ -1,41 +1,50 @@
 package com.bharath.flightreservation.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-import java.util.Date;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "RESERVATION")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity
 public class Reservation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "CHECKED_IN")
     private Boolean checkedIn;
+
+    @Column(name = "NUMBER_OF_BAGS")
     private int numberOfBags;
 
-    @ManyToOne
-    @JoinColumn(name = "passenger_id", nullable = false)
+    @OneToOne
+    @JoinColumn(name = "PASSENGER_ID", nullable = false)
     private Passenger passenger;
 
-    @ManyToOne
-    @JoinColumn(name = "flight_id", nullable = false)
+    @OneToOne
+    @JoinColumn(name = "FLIGHT_ID", nullable = false)
     private Flight flight;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date created;
+    @CreationTimestamp
+    @Column(name = "CREATED", nullable = false, updatable = false, insertable = false)
+    private LocalDateTime created;
+
 }
