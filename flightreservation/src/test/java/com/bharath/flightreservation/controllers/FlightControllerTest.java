ca259bbb7751e98620292d1689a64f7923df4853
PierreSQS <nomo_messina@web.de>
2025-02-04 12:53:58 +0100

    Sec6_Chap47-XXX: optimized MVC-Controller test

--- flightreservation/src/test/java/com/bharath/flightreservation/controllers/FlightControllerTest.java
+++ flightreservation/src/test/java/com/bharath/flightreservation/controllers/FlightControllerTest.java
@@ -3,38 +3,40 @@ package com.bharath.flightreservation.controllers;
 import com.bharath.flightreservation.repositories.FlightRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-
+import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@WebMvcTest(controllers = {FlightController.class})
 class FlightControllerTest {
 
-    @Mock
+    @MockitoBean
     private FlightRepository flightRepository;
 
-    @InjectMocks
-    private FlightController flightController;
-
+    @Autowired
     private MockMvc mockMvc;
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(flightController).build();
+
     }
 
     @Test
     void displayFindFlightReturnsFindFlightsView() throws Exception {
         mockMvc.perform(get("/findFlights"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("findFlights"));
+                .andExpect(view().name("findFlights"))
+                .andExpect(content()
+                        .string(containsString("<title>Find Flights</title>")))
+                .andDo(print());
     }
 
 }
