d2be81408b10101fab96a4bbb8780a450d5dd96d
PierreSQS <nomo_messina@web.de>
2025-02-04 08:19:51 +0100

    Sec6_Chap45: refined generated entities

--- flightreservation/src/main/java/com/bharath/flightreservation/entities/Flight.java
+++ flightreservation/src/main/java/com/bharath/flightreservation/entities/Flight.java
@@ -1,9 +1,11 @@
 package com.bharath.flightreservation.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,15 +19,28 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Entity
+@Table(name = "FLIGHT")
 public class Flight {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private Long id;
+
+    @Column(name = "FLIGHT_NUMBER")
     private String flightNumber;
+
+    @Column(name = "OPERATING_AIRLINES")
     private String operatingAirline;
+
+    @Column(name = "DEPARTURE_CITY")
     private String departureCity;
+
+    @Column(name = "ARRIVAL_CITY")
     private String arrivalCity;
+
+    @Column(name = "DATE_OF_DEPARTURE")
     private LocalDateTime dateOfDeparture;
+
+    @Column(name = "ESTIMATED_DEPARTURE_TIME")
     private LocalDateTime estimatedDepartureTime;
 }
--- flightreservation/src/main/java/com/bharath/flightreservation/entities/Passenger.java
+++ flightreservation/src/main/java/com/bharath/flightreservation/entities/Passenger.java
@@ -1,9 +1,11 @@
 package com.bharath.flightreservation.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,14 +20,26 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 @Entity
+@Table(name = "PASSENGER")
 public class Passenger {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     private Long id;
+
+    @Column(name = "FIRST_NAME")
     private String firstName;
+
+    @Column(name = "LAST_NAME")
     private String lastName;
+
+    @Column(name = "MIDDLE_NAME")
     private String middleName;
+
+    @Column(name = "EMAIL")
     private String email;
+
+    @Column(name = "PHONE")
     private String phone;
 
 }
