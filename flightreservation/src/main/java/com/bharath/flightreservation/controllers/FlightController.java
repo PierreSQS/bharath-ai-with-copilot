2163912742f543bc0d71809bc57315c9cd752664
PierreSQS <nomo_messina@web.de>
2025-02-04 16:18:04 +0100

    Sec6_Chap48-NOK: implemented findFlights-handler

--- flightreservation/src/main/java/com/bharath/flightreservation/controllers/FlightController.java
+++ flightreservation/src/main/java/com/bharath/flightreservation/controllers/FlightController.java
@@ -1,21 +1,38 @@
 package com.bharath.flightreservation.controllers;
 
+import com.bharath.flightreservation.entities.Flight;
 import com.bharath.flightreservation.repositories.FlightRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
-// TODO: eventually remove the annotation below
-// @RequestMapping("/flights")
+
 public class FlightController {
 
     private final FlightRepository flightRepository;
 
     @GetMapping("/findFlights")
-    public String displayFindFlight() {
+    public String displayFindFlightForm() {
         return "findFlights";
+    }
+
+    @PostMapping("/findFlights")
+    public String findFlights(@RequestParam("to") String to,
+                              @RequestParam("from") String from,
+                              @RequestParam("departureDate") LocalDate departureDate,
+                              Model model) {
+        List<Flight> flights = flightRepository
+                .findByDepartureCityAndArrivalCityAndDateOfDeparture(from, to, departureDate.atStartOfDay());
+        model.addAttribute("flights", flights);
+        return "displayFlights";
     }
 
 }
