03870d60d3677de47bb5fd03847ed9ef76144452
PierreSQS <nomo_messina@web.de>
2025-02-04 23:49:10 +0100

    Sec6_Chap49: implemented find flights in repo

--- flightreservation/src/main/java/com/bharath/flightreservation/repositories/FlightRepository.java
+++ flightreservation/src/main/java/com/bharath/flightreservation/repositories/FlightRepository.java
@@ -4,6 +4,10 @@ import com.bharath.flightreservation.entities.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+    List<Flight> findByDepartureCityAndArrivalCityAndDateOfDeparture(String from, String to, LocalDate departureDate);
 }
