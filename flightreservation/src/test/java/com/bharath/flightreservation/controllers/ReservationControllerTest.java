4f4a834547156973d8e23bfeb5c53049b0a88b2f
PierreSQS <nomo_messina@web.de>
2025-02-05 08:46:41 +0100

    Sec6_Chap51-XXX: optimized Controller test

--- flightreservation/src/test/java/com/bharath/flightreservation/controllers/ReservationControllerTest.java
+++ flightreservation/src/test/java/com/bharath/flightreservation/controllers/ReservationControllerTest.java
@@ -2,8 +2,8 @@ package com.bharath.flightreservation.controllers;
 
 import com.bharath.flightreservation.entities.Flight;
 import com.bharath.flightreservation.repositories.FlightRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -13,10 +13,14 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -25,29 +29,40 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ReservationControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
 
     @MockitoBean
-    private FlightRepository flightRepository;
+    FlightRepository flightRepository;
+
+    Flight flightMock;
+
+    @BeforeEach
+    void setUp() {
+        flightMock = new Flight();
+        flightMock.setId(1L);
+        flightMock.setDepartureCity("NYC");
+        flightMock.setArrivalCity("LAX");
+    }
 
     @Test
     void testDisplayReservationForm() throws Exception {
-        Flight flight = new Flight();
-        flight.setId(1L);
-        flight.setDepartureCity("NYC");
-        flight.setArrivalCity("LAX");
+        // Given
+        given(flightRepository.findById(anyLong()))
+                .willReturn(Optional.of(flightMock));
 
-        Mockito.when(flightRepository.findById(anyLong())).thenReturn(Optional.of(flight));
-
+        // When, Then
         MvcResult result = mockMvc.perform(get("/displayReservationForm")
                         .param("flightID", "1"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("completeReservation"))
-                .andExpect(model().attributeExists("flight"))
-                .andExpect(model().attribute("flight", is(flight)))
+                .andExpect(model().attribute("flight", is(flightMock)))
+                .andExpect(content()
+                        .string(containsString("<title>Complete Reservation</title>")))
+                .andDo(print())
                 .andReturn();
 
         ModelAndView modelAndView = result.getModelAndView();
+        assert modelAndView != null;
         Flight modelFlight = (Flight) modelAndView.getModel().get("flight");
         assertEquals("NYC", modelFlight.getDepartureCity());
         assertEquals("LAX", modelFlight.getArrivalCity());
