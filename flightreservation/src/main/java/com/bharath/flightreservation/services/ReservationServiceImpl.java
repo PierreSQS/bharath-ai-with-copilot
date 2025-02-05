d4b77bdbdeba890d33434e7971f98d44d05a0908
PierreSQS <nomo_messina@web.de>
2025-02-05 13:04:16 +0100

    Sec6_Chap54: implemented bookFlight() Serv. Operation

--- flightreservation/src/main/java/com/bharath/flightreservation/services/ReservationServiceImpl.java
+++ flightreservation/src/main/java/com/bharath/flightreservation/services/ReservationServiceImpl.java
@@ -1,11 +1,45 @@
 package com.bharath.flightreservation.services;
 
 import com.bharath.flightreservation.dtos.ReservationDTO;
+import com.bharath.flightreservation.entities.Flight;
+import com.bharath.flightreservation.entities.Passenger;
 import com.bharath.flightreservation.entities.Reservation;
+import com.bharath.flightreservation.repositories.FlightRepository;
+import com.bharath.flightreservation.repositories.PassengerRepository;
+import com.bharath.flightreservation.repositories.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class ReservationServiceImpl implements ReservationService {
+
+    private final FlightRepository flightRepository;
+
+    private final PassengerRepository passengerRepository;
+
+    private final ReservationRepository reservationRepository;
+
     @Override
     public Reservation bookFlight(ReservationDTO reservationReqDTO) {
-        return null;
+        // find the flight by requested flight id
+        Flight foundFlight = flightRepository.findById(reservationReqDTO.getId())
+                .orElseThrow(() -> new RuntimeException("Flight not found"));
+
+        // create a new Passenger object set the field
+        // with RequestDTO and save it to the database
+        Passenger savedPassenger = passengerRepository.save(Passenger.builder()
+                .firstName(reservationReqDTO.getFirstName())
+                .lastName(reservationReqDTO.getLastName())
+                .phone(reservationReqDTO.getPhone())
+                .email(reservationReqDTO.getEmail())
+                .build());
+
+        // create a Reservation object, set the savedPassenger and foundFlight
+        // with the saved passenger object and save it to the database
+        return reservationRepository.save(Reservation.builder()
+                .passenger(savedPassenger)
+                .flight(foundFlight)
+                .build());
     }
 }
