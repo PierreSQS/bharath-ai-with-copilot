df93d2f27dae7d60e8a7317bfe1af039ac20e66c
PierreSQS <nomo_messina@web.de>
2025-03-09 15:03:32 +0100

    Sec6_Chap43-XXX: corrected DB User

--- flightreservation/src/main/resources/scripts/mysql-init.sql
+++ flightreservation/src/main/resources/scripts/mysql-init.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS reservation_db;
-DROP USER IF EXISTS `fligthadmin`@`localhost`;
+DROP USER IF EXISTS `flightadmin`@`localhost`;
 CREATE DATABASE IF NOT EXISTS reservation_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS `fligthadmin`@`localhost` IDENTIFIED WITH mysql_native_password BY 'fligthadmin';
+CREATE USER IF NOT EXISTS `flightadmin`@`localhost` IDENTIFIED WITH mysql_native_password BY 'flightadmin';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
-CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `reservation_db`.* TO `fligthadmin`@`localhost`;
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `reservation_db`.* TO `flightadmin`@`localhost`;
